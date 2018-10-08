d018dc4656a9eb67354d7f6619f073dc164dce2e
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-10-08 15:33:01 -0700

    working

--- init.py
+++ init.py
@@ -19,7 +19,7 @@ def install_software():
 
 def get_deployed_apps_from_consul():
   print("scraping consul for deployed apps")
-  url = 'http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/?keys&separator=/'
+  url = 'http://consul:8500/v1/kv/?keys&separator=/'
   response = requests.get(url)
   toplevel_keys_json = json.loads(response.text)
   return toplevel_keys_json
@@ -30,30 +30,30 @@ def retrieve_app_configs_from_consul(toplevel_keys_json):
   for x in toplevel_keys_json:
     project_name = x.strip('/')
     print("[{}] pulling consul configs".format(project_name))
-    aws_account_number_url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/AWS_ACCOUNT_NUMBER?raw".format(project_name)
+    aws_account_number_url = "http://consul:8500/v1/kv/{}/config/AWS_ACCOUNT_NUMBER?raw".format(project_name)
     response_aws_account_number = requests.get(aws_account_number_url)
     aws_account_number = response_aws_account_number.text
 
-    ecs_cluster_url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/ecs_cluster?raw".format(project_name)
+    ecs_cluster_url = "http://consul:8500/v1/kv/{}/config/ecs_cluster?raw".format(project_name)
     response_ecs_cluster = requests.get(ecs_cluster_url)
     ecs_cluster = response_ecs_cluster.text
 
-    branch_url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/branch?raw".format(project_name)
+    branch_url = "http://consul:8500/v1/kv/{}/config/branch?raw".format(project_name)
     response_branch_url = requests.get(branch_url)
     test1 = response_branch_url.status_code
     branch = response_branch_url.text
 
-    github_url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/github_repo?raw".format(project_name)
+    github_url = "http://consul:8500/v1/kv/{}/config/github_repo?raw".format(project_name)
     response_github_url = requests.get(github_url)
     test2 = response_github_url.status_code
     github_repo = response_github_url.text
 
-    ecr_repo_url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/ecr_repo?raw".format(project_name)
+    ecr_repo_url = "http://consul:8500/v1/kv/{}/config/ecr_repo?raw".format(project_name)
     response_ecr_repo = requests.get(ecr_repo_url)
     test3 = response_ecr_repo.status_code
     ecr_repo = response_ecr_repo.text
     if ecr_repo:
-      ecr_image_digest_url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/ecr_image_digest?raw".format(project_name)
+      ecr_image_digest_url = "http://consul:8500/v1/kv/{}/config/ecr_image_digest?raw".format(project_name)
       response_ecr_image_digest = requests.get(ecr_image_digest_url)
       if re.match('^sha256:[a-zA-Z0-9]*$', response_ecr_image_digest.text):
         if response_ecr_image_digest.status_code == 200:
@@ -76,16 +76,16 @@ def whats_in_ecr(app_list, app_list_dict):
     print("[{}] pull imageDigest from ecr".format(k))
     try:
       response = client.describe_images(registryId=app_list_dict[k][0], repositoryName=app_list_dict[k][4], imageIds=[{'imageTag': app_list_dict[k][2]}])
-    except:
-      print("[{}] something went wrong w/ the ecr image lookup".format(k))
+    except Exception as errormsg:
+      print("[{}] something went wrong w/ the ecr image lookup ({})".format(k, errormsg))
       aws_ecr_image_digest = ""
       app_list_dict[k].append(aws_ecr_image_digest)
-    aws_ecr_image_digest = response['imageDetails'][0]['imageDigest']
-    app_list_dict[k].append(aws_ecr_image_digest)
+    else:
+      aws_ecr_image_digest = response['imageDetails'][0]['imageDigest']
+      app_list_dict[k].append(aws_ecr_image_digest)
 
 def container_restart_logic(app_list_dict):
   for k,v in app_list_dict.items():
-    print("[{}] container restart logic".format(k))
     if not app_list_dict[k][6]:
       print("[{}] aws ecr_image_digest is missing, skipping".format(k))
       continue
@@ -95,19 +95,23 @@ def container_restart_logic(app_list_dict):
     if app_list_dict[k][5] != app_list_dict[k][6]:
       print("[{}] aws ecr_image_digest differs from consul's ecr_image_digest".format(k))
       container_restart_status = restart_containers(app_list_dict[k])
+      update_consul = True
       for status in container_restart_status:
         if status != 200:
           print("[{}] one container didn't properly restart, not updating consul".format(k))
-        else:
-          print("[{}] all containers successfully restarted, updating consul".format(k))
-          update_consul_ecr_image_digest(app_list_dict)
+          update_consul = False
+    if update_consul == True:
+      update_consul_ecr_image_digest(app_list_dict, k)
+      del(update_consul)
+    else:
+      del(update_consul)
 
-def update_consul_ecr_image_digest(app_list_dict):
-  for k,v in app_list_dict.items():
-    print("[{}] updating consul ecr_image_digest".format(k))
-    url = "http://consul.user1.media.dev.usa.reachlocalservices.com:8500/v1/kv/{}/config/ecr_image_digest".format(k)
-    response = requests.put(url, data=app_list_dict[k][6])
-    print("CONSUL RESPONSE: ", response.status_code, response.reason)
+def update_consul_ecr_image_digest(app_list_dict, key):
+  #for k,v in app_list_dict.items():
+  print("[{}] updating consul ecr_image_digest".format(key))
+  url = "http://consul:8500/v1/kv/{}/config/ecr_image_digest".format(key)
+  response = requests.put(url, data=app_list_dict[key][6])
+  print("CONSUL RESPONSE: ", response.status_code, response.reason)
 
 def restart_containers(something):
   print("[{}] restarting containers".format(something[4]))
